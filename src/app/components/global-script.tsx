f43805f0573a1221397c186d9fbd8e7407f2d4c9
Quarticdot AI <makadsuhel11@gmail.com>
2024-11-23 18:39:31 +0530

    Add google tag manager

--- src/app/components/global-script.tsx
+++ src/app/components/global-script.tsx
@@ -1,3 +1,10 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { GOOGLE_TAG_ID } from "@/lib/constants";
+
 export const GlobalScript = () => {
-  return <></>;
+  return (
+    <>
+      <GoogleAnalytics gaId={GOOGLE_TAG_ID} />
+    </>
+  );
 };
---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const GOOGLE_TAG_ID = "G-KCZZ11PTY6";
