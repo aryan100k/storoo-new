85d6beb81f41d1145f74964fa62f96673a63e2b1
Quarticdot AI <makadsuhel11@gmail.com>
2024-11-25 23:15:34 +0530

    move public page to grouped folder

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,9 +2,10 @@ import { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
 
 import { Toaster } from "@/components/ui/sonner";
-import { TRPCProvider } from "./(public)/components/trpc-provider";
 
-import "./globals.css";
+import { TRPCProvider } from "./components/trpc-provider";
+import { GlobalScript } from "./components/global-script";
+import { Navbar } from "./components/navbar";
 
 export const metadata: Metadata = {
   title: "Storoo",
@@ -16,12 +17,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/storoo-logo-white.svg" />
+
+        <GlobalScript />
       </head>
 
       <body className="bg-background font-sans text-primary antialiased">
         <TRPCProvider>
           <NextTopLoader showSpinner={false} />
           <Toaster />
+
+          <header className="bg-white shadow-sm sticky top-0 z-10 isolate">
+            <Navbar />
+          </header>
 
           {children}
         </TRPCProvider>
