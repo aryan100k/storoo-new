33f44b264474e550587f79e74b371c4b3420e2fa
Quarticdot AI <makadsuhel11@gmail.com>
2024-11-26 22:24:55 +0530

    init parnter and storage pages

--- src/app/admin/partner-listings/page.tsx
+++ src/app/admin/partner-listings/page.tsx
@@ -1,5 +1,22 @@
+import { Breadcrumbs } from "../components/breadcrumbs";
+import { Heading } from "../components/heading";
+import { adminRoutes } from "@/lib/routes";
+
 const PartnerListingsPage = () => {
-  return <div>PartnerListingsPage</div>;
+  return (
+    <>
+      <Breadcrumbs
+        items={[
+          {
+            label: "Partner Requests",
+            href: adminRoutes.partnerListings,
+          },
+        ]}
+      />
+
+      <Heading>Partner Requests</Heading>
+    </>
+  );
 };
 
 export default PartnerListingsPage;
--- src/app/admin/storage-points/page.tsx
+++ src/app/admin/storage-points/page.tsx
@@ -1,5 +1,22 @@
+import { Breadcrumbs } from "../components/breadcrumbs";
+import { Heading } from "../components/heading";
+import { adminRoutes } from "@/lib/routes";
+
 const StoragePointsPage = () => {
-  return <div>StoragePointsPage</div>;
+  return (
+    <>
+      <Breadcrumbs
+        items={[
+          {
+            label: "Storage Points",
+            href: adminRoutes.storagePoints,
+          },
+        ]}
+      />
+
+      <Heading>Storage Points</Heading>
+    </>
+  );
 };
 
 export default StoragePointsPage;
