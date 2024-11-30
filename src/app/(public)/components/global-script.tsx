d4f075dbb2d826329e8e24d815644ea2453a2401
Suhel Makkad <makadsuhel11@gmail.com>
2024-11-30 23:45:04 +0530

    add fb event id

--- src/app/(public)/components/global-script.tsx
+++ src/app/(public)/components/global-script.tsx
@@ -1,10 +1,38 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { GOOGLE_TAG_ID } from "@/lib/constants";
+import { FB_PIXEL_ID, GOOGLE_TAG_ID } from "@/lib/constants";
+import Script from "next/script";
 
 export const GlobalScript = () => {
   return (
     <>
       <GoogleAnalytics gaId={GOOGLE_TAG_ID} />
+
+      <Script
+        id="facebook-fb-events"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${FB_PIXEL_ID}');
+fbq('track', 'PageView');
+`,
+        }}
+      />
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
     </>
   );
 };
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,5 @@
 export const GOOGLE_TAG_ID = "G-KCZZ11PTY6";
+
+export const FB_PIXEL_ID = "613577147668218";
+
 export const googleMapAPIKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
