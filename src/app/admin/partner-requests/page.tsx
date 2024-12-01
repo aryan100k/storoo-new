3ef8bd015edd2919e756dc41b0100c9450790218
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-12-01 10:55:16 -0500

    Add status filter component to partner requests page

--- src/app/admin/partner-requests/page.tsx
+++ src/app/admin/partner-requests/page.tsx
@@ -7,21 +7,26 @@ import { adminRoutes } from "@/lib/routes";
 import { requestsColumns } from "./components/columns";
 import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
+import { StatusSelect, useStatus } from "./components/status-select";
 
 const partnersPerPage = 8;
 
 const PartnerRequestsPage = () => {
+  const selectedStatus = useStatus();
+
   const { isLoading: isTotalLoading, data: totalRequests = 0 } =
     trpc.getTotalPartnershipRequestsByRequest.useQuery({});
-  const { isLoading, data, fetchNextPage } = trpc.getPartnershipRequests.useInfiniteQuery(
-    {
-      limit: partnersPerPage,
-    },
-    {
-      initialCursor: 0,
-      getNextPageParam: (_, pages) => pages.flatMap((page) => page).length,
-    }
-  );
+  const { isLoading, data, fetchNextPage } =
+    trpc.getPartnershipRequests.useInfiniteQuery(
+      {
+        limit: partnersPerPage,
+        // status: selectedStatus,
+      },
+      {
+        initialCursor: 0,
+        getNextPageParam: (_, pages) => pages.flatMap((page) => page).length,
+      }
+    );
 
   const requests = data?.pages.flatMap((page) => page) ?? [];
   const hasMore = data?.pages.length === 0 || requests.length < totalRequests;
@@ -31,15 +36,22 @@ const PartnerRequestsPage = () => {
       <Breadcrumbs
         items={[
           {
-            label: "Partner Listings",
-            href: adminRoutes.partnerListings,
+            label: "Partner Requests",
+            href: adminRoutes.partnerRequests,
           },
         ]}
       />
 
-      <Heading>Partner Requests</Heading>
+      <div className="flex justify-between flex-col md:flex-row">
+        <Heading className="mb-0">Partner Requests</Heading>
+        <StatusSelect />
+      </div>
 
-      <DataTable columns={requestsColumns} data={requests} isLoading={isLoading} />
+      <DataTable
+        columns={requestsColumns}
+        data={requests}
+        isLoading={isLoading}
+      />
 
       <div className="flex justify-end mt-2 gap-2">
         {!isTotalLoading && (
